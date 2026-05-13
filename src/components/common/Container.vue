e170844635e63965e81ad8e91a26eadb24be913f
arthaajoshipurba-cyber <arthaajoshipurba@gmail.com>
2026-05-13 19:07:21 +0700

    Create Container.vue

---
+++ src/components/common/Container.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :class="['container mx-auto px-4 sm:px-6 lg:px-8', className]">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  className?: string
+}>()
+</script>
