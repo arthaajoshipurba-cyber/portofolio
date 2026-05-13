10f9ac39441e98daa1c05733491b98cb34085489
arthaajoshipurba-cyber <arthaajoshipurba@gmail.com>
2026-05-13 21:55:47 +0700

    Update socialLinks.ts

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,8 +1,9 @@
-import { Github, TikTok, Mail, Instagram } from 'lucide-vue-next'
+import { Github, TikTok, Twitter, Mail, Instagram } from 'lucide-vue-next'
 
 export default [
   { name: 'GitHub', url: 'https://github.com/arthaajoshipurba-cyber', icon: Github },
   { name: 'TikTok', url: 'https://tiktok.com/@xxboyss31', icon: Tiktok },
+  { name: 'Twitter', url: 'https://twitter.com/gadaktwitterbang', icon: Twitter },
   { name: 'Email', url: 'mailto:arthaa156@gmail.com', icon: Mail },
   { name: 'Instagram', url: 'https://instagram.com/attchx_', icon: Instagram }
 ]
