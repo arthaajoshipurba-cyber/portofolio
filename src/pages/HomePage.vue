8db359beb917e15f63e2af39709cb973e0323888
arthaajoshipurba-cyber <arthaajoshipurba@gmail.com>
2026-05-13 19:20:01 +0700

    Create HomePage.vue

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <HeroSection />
+    <AboutSection />
+    <ProjectsSection />
+    <TechStackSection />
+    <ExperienceSection />
+    <ContactSection />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeroSection from '@/components/home/HeroSection.vue'
+import AboutSection from '@/components/home/AboutSection.vue'
+import ProjectsSection from '@/components/home/ProjectsSection.vue'
+import TechStackSection from '@/components/home/TechStackSection.vue'
+import ExperienceSection from '@/components/home/ExperienceSection.vue'
+import ContactSection from '@/components/home/ContactSection.vue'
+</script>
