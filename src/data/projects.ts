acfdba3f490ceae972e7c97fcdb50d3be36d5556
arthaajoshipurba-cyber <arthaajoshipurba@gmail.com>
2026-06-15 20:42:36 +0700

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,14 +9,4 @@ export const projects = [
     github: "https://github.com/arthaajoshipurba-cyber/portofolio",
     liveDemo: "https://arthajoshii.vercel.app/"
   }
-  {
-    id: 2,
-    title: "Portofolio Artha",
-    description: "Portofolio Artha",
-    image: "https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=800&h=600&fit=crop",
-    tech: ["Vue.js", "Tailwind", "CSS", "React"],
-    category: "Ubnnbbsite"
-    github: "https://github.com/arthaajoshipurba-cyber/portofolio",
-    liveDemo: "https://arthajoshii.vercel.app/"
-  }
 ]
